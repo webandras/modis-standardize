166ffa01a738eca1dcec2b48685199e9f112b3cd
Andras Gulacsi <guland@protonmail.com>
2020-01-15 16:40:31 +0100

    module is ready and tested

--- modis/app.py
+++ modis/app.py
@@ -12,7 +12,8 @@ products = Products()
 
 # Function to run the app with '__main__.py'
 def run():
-  index = 'ET'
+  # SET THIS VARIABLE!
+  index = 'LST_Night_1km'
   # NDDI, NDVI, NDWI (2000, DOY 64)  => 89
   # ET (2001, DOY 1) => 89
   # LST_Day_1km, LST_Night_1km (2000, DOY 64) => 89
@@ -21,7 +22,9 @@ def run():
   # set day of year in this variable:
   doyStart = 89
 
-  year = 2001
+  # SET THESE VARIABLES!
+  year = 2000
+  endYear = 2020
 
   # Data resolution
   resolution = 500
@@ -30,7 +33,8 @@ def run():
   step = 7
   
   # MODIS product name, 'products' object holds the values
-  data = products.modisEt
+  # SET THIS VARIABLE!
+  data = products.modisTemp
 
   # modisRefl = 'MODIS/006/MOD09A1' # => NDDI
   # modisTemp = 'MODIS/006/MOD11A2' # => T/NDVI
@@ -40,7 +44,8 @@ def run():
 
   print('MODIS termék azonosító:  ' + str(data) + '.')
   print('A választott index: ' + str(index) + '.')
-  print('A választott év: ' + str(year) + '.')
+  print('A választott kezdőév: ' + str(year) + '.')
+  print('A választott befejezőév: ' + str(endYear) + '.')
   print('Térbeli felbontás: ' + str(resolution) + ' m.')
   print('Időbeli felbontás: ' + str((step + 1)) + ' nap.')
 
@@ -49,49 +54,54 @@ def run():
   print('++++++++++++++++++++++++++++++++++++++')
   print('\n')
 
-  # Looping through day of year values, processing each doy periods
-  for current in range(doyStart, 281, 8):
-    doyStart = current
-    print('A választott DOY: ' + str(doyStart))
+  for currYear in range(year, endYear, 1):
+    # Looping through day of year values, processing each doy periods
+    currDoy = doyStart
+    while currDoy < 281:
+      print('Az aktuális év: ' + str(currYear))
+      print('Az aktuális DOY: ' + str(currDoy))
 
-    # HUN+SRB study area.
-    studyArea = ee.FeatureCollection('users/gulandras90/shapefiles/study_area_modis')
+      # HUN+SRB study area.
+      studyArea = ee.FeatureCollection('users/gulandras90/shapefiles/study_area_modis')
 
-    # Parameters for filtering the ImageCollection
-    point = ee.Geometry.Point(19.072, 47.204)
+      # Parameters for filtering the ImageCollection
+      point = ee.Geometry.Point(19.072, 47.204)
 
-    # Reference period
-    start = ee.Date('2000-01-01')
-    finish = ee.Date('2019-12-31')
+      # Reference period
+      start = ee.Date('2000-01-01')
+      finish = ee.Date('2019-12-31')
 
-    if index == 'LST_Night_1km':
-      finalResult = calculateMeanStdev(data, index, doyStart, step,
-        start, finish, point, studyArea, resolution, True)
-    else:
-      finalResult = calculateMeanStdev(data, index, doyStart, step,
-        start, finish, point, studyArea, resolution, False)
+      if index == 'LST_Night_1km':
+        finalResult = calculateMeanStdev(data, index, currDoy, step,
+          start, finish, point, studyArea, resolution, True)
+      else:
+        finalResult = calculateMeanStdev(data, index, currDoy, step,
+          start, finish, point, studyArea, resolution, False)
 
-    if index == 'LST_Night_1km':
-      standardizedResult = standardizeVariables(data, index, year, doyStart, step, point, finalResult['meanMap'], finalResult['stdevMap'], resolution, True)
-    else:
-      standardizedResult = standardizeVariables(data, index, year, doyStart, step, point, finalResult['meanMap'], finalResult['stdevMap'], resolution, False)
+      if index == 'LST_Night_1km':
+        standardizedResult = standardizeVariables(data, index, currYear, currDoy, step, point, finalResult['meanMap'], finalResult['stdevMap'], resolution, True)
+      else:
+        standardizedResult = standardizeVariables(data, index, currYear, currDoy, step, point, finalResult['meanMap'], finalResult['stdevMap'], resolution, False)
 
-    task_config = {
-      'fileNamePrefix': str(index) + '_' + str(resolution) + 'm_' + str(year) + '_doy' + str(doyStart),
-      'crs': 'EPSG:4326',
-      'scale': 500,
-      'maxPixels': 10e10,
-      'fileFormat': 'GeoTIFF',
-      'skipEmptyTiles': True,
-      'region': studyArea.geometry().getInfo()['coordinates'] ,
-      'folder': 'modis_python/' + str(index)
-    }
+      task_config = {
+        'fileNamePrefix': str(index) + '_' + str(resolution) + 'm_' + str(currYear) + '_doy' + str(currDoy),
+        'crs': 'EPSG:4326',
+        'scale': 500,
+        'maxPixels': 10e10,
+        'fileFormat': 'GeoTIFF',
+        'skipEmptyTiles': True,
+        'region': studyArea.geometry().getInfo()['coordinates'] ,
+        'folder': 'modis_python/' + str(index)
+      }
 
-    # save results to Google Drive
-    task = ee.batch.Export.image.toDrive(standardizedResult, str('standardized') + str(index) + str(current), **task_config)
-    task.start()
-    print('A feldolgozás elindult... Eredmény a Google Drive-ra mentve.')
-    print('--------------------------------------')
-    print('\n')
+      # save results to Google Drive
+      task = ee.batch.Export.image.toDrive(standardizedResult, str('standardized') + str(index) + str(currDoy), **task_config)
+      task.start()
+      print('A feldolgozás elindult... Az eredmény a Google Drive-ra lesz mentve.')
+      print('--------------------------------------')
+      print('\n')
 
-print('A feldolgozás sikeresen elindult...')
+      currDoy = currDoy + 8
+
+
+print('A feldolgozás sikeresen elindult... Eltart egy darabig.')
